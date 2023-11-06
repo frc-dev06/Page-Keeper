7e06c022562dde777ad42c2bfb064ac1ac83556e
Manuel Alejandro Cortes Cardenas <82416755+MACC-dev@users.noreply.github.com>
2023-11-05 21:19:56 -0500

    Add files via upload

--- src/controllers/correoController.js
+++ src/controllers/correoController.js
@@ -1,4 +1,6 @@
 const express= require('express')
+const nodemailer = require('nodemailer');
+
 
 
 function checkSession(req, res){
@@ -9,6 +11,40 @@ function checkSession(req, res){
     }
 }
 
-module.exports={
-    checkSession
+const sendEmail = (req, res) => {
+    const { email, subject, message, file } = req.body;
+
+    // Configura tu transporte de correo aquí
+    let transporter = nodemailer.createTransport({
+        // Tus configuraciones de transporte
+    });
+
+    // Configura tu correo aquí
+    let mailOptions = {
+        from: 'tu-correo@gmail.com',
+        to: email,
+        subject: subject,
+        text: message,
+        attachments: [
+            {
+                path: file // Asegúrate de manejar la carga de archivos correctamente
+            }
+        ]
+    };
+
+    // Envía el correo
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send('error');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.status(200).send('success');
+        }
+    });
+};
+
+module.exports = {
+    checkSession,
+    sendEmail
 }
