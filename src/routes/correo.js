388488377c0af374b14b5682415b4117b87cbffe
Manuel Alejandro Cortes Cardenas <82416755+MACC-dev@users.noreply.github.com>
2023-11-05 21:21:40 -0500

    Add files via upload

--- src/routes/correo.js
+++ src/routes/correo.js
@@ -4,5 +4,6 @@ const correoController = require('../controllers/correoController');
 const router= express.Router();
 
 router.get('/', correoController.checkSession);
+router.post('/send-email', correoController.sendEmail);
 
 module.exports= router;
--- src/routes/ventas.js
+++ src/routes/ventas.js
@@ -1,8 +1,11 @@
 const express= require('express');
 const ventasCotroller= require('../controllers/ventasCotroller');
+const { route } = require('./login');
 
 const router= express.Router();
 
 router.get('/', ventasCotroller.checkSession);
+router.get('/search', ventasCotroller.search);
+router.post('/buy', ventasCotroller.buy);
 
 module.exports= router
